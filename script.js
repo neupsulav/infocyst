24cd93fc208fec1223baad074bf4c563b8ec4292
Sulav Neupane <neupsulav@gmail.com>
2023-01-19 11:14:24 +0545

    Respo navbar added

---
+++ script.js
@@ -0,0 +1,13 @@
+let hamburger = document.querySelector('.hamburger')
+let navBar = document.getElementsByTagName('nav')[0]
+let links = document.getElementById('links')
+let ul = document.getElementsByClassName('ul')[0]
+
+hamburger.addEventListener('click', () => {
+    links.classList.toggle('links-show')
+    navBar.classList.toggle('expand')
+    navBar.style.transition = '1s'
+    setTimeout(() => {
+        ul.classList.toggle('links-expand')
+    }, 500);
+})
